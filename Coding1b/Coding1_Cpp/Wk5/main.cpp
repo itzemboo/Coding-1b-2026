eaf345ef143796640bcfb9e32765b27ca5aa28d2
itzemboo <miabalicki77@gmail.com>
2026-05-14 20:09:59 -0500

    Wk5

---
+++ Coding1b/Coding1_Cpp/Wk5/main.cpp
@@ -0,0 +1,37 @@
+// Mia Balicki COding 1b Week 15 
+// Class 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// classes are custom variable types 
+// varibles 
+//functions 
+
+//contructon
+//getters and setters ()
+
+class robot{
+public: 
+string name; 
+
+}; // end of robot class
+
+int main() {
+    cout << "Classes!\n";
+
+    //create our first instance of a robot
+    robot artoo;
+    artoo.name = "R2-D2";       //dot syntax
+    artoo.charge = "C-3PO";
+
+    robot threepio;
+    threepio.name = "C-3PO"
+
+
+    return 0;
+
+
+
+}
